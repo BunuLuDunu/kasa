97c0a3936bf177704c22c471fcc9a6cfb21ead9a
BunuLuDunu <laurine.sassano@gmail.com>
2023-07-08 17:24:02 +0200

    Added styled header & global font

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,22 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import Logo from '../../assets/logo.png'
+import '../Header/header.scss'
 
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A propos</Link>
-        </nav>
+        <header>
+            <div className="header-wrapper">
+                <NavLink to="/">
+                    <img src={Logo} alt="Kasa logo"/>
+                </NavLink>
+                <nav>
+                    <ul className="nav-menu">
+                        <li><NavLink to="/" className="nav-link">Accueil</NavLink></li>
+                        <li><NavLink to="/about" className="nav-link">A propos</NavLink></li>
+                    </ul>
+                </nav>
+            </div> 
+        </header>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './assets/fonts/Montserrat-Medium.ttf';
+import './index.scss';
 import Home from './pages/Home/home.jsx';
-import About from './pages/About/about.jsx'
-import Error from './pages/Error/error.jsx'
-import Header from './components/Header/header.jsx'
-import Footer from './components/Footer/footer.jsx'
+import About from './pages/About/about.jsx';
+import Error from './pages/Error/error.jsx';
+import Header from './components/Header/header.jsx';
+import Footer from './components/Footer/footer.jsx';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);

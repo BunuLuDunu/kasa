215eda25165d2b34059d6c81ff1a982eec197cbf
BunuLuDunu <laurine.sassano@gmail.com>
2023-07-12 15:19:10 +0200

    Added 404 page layout & Home page banner

--- src/pages/Error/error.jsx
+++ src/pages/Error/error.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+import '../Error/error.scss'
+
 function Error() {
     return (
-        <div>
-            <h1>Oups</h1>
-        </div>
+        <main className="error-wrapper">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to='/'>Retourner sur la page d'accueil</Link>
+        </main>
     )
 }
 
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,8 +1,20 @@
+import Banner from '../../assets/img/home-banner.png'
+import '../Home/home.scss'
+
 function Home() {
     return (
-        <div>
-            <h1>Page d'accueil</h1>
-        </div>
+        <main className='home-container'>
+           <section className="home-banner">
+                <img src={Banner} alt="Vagues s'échouant sur des côtes rocheuses en bord de forêt" />
+                <h1>Chez vous, partout et ailleurs</h1>
+            </section>
+
+            <section className='housings'>
+                <ul>
+                    <li></li>
+                </ul>
+            </section>
+        </main>
     )
 }
 

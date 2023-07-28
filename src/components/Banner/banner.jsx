d2373ce3d850ad8b14819e6c5ecd9c8daea3d714
BunuLuDunu <laurine.sassano@gmail.com>
2023-07-28 09:18:01 +0200

    Added comments for readability

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -4,6 +4,7 @@ function Banner({img, title}) {
     return (
         <section className="banner">
             <img src={img.src} alt={img.alt} />
+            {/* Condition pour vérifier si un titre est présent */}
             {title ? (
                 <h1>{title}</h1>
             ) : ''}
